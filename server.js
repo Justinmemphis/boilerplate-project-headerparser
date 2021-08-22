7dc86dbf8c536dae1536ba3045da7babda5bbb1b
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-08-21 21:21:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 // server.js
 // where your node app starts
 
+/*
+To Do:
+
+*/
+
 // init project
 require('dotenv').config();
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -20,7 +25,7 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
